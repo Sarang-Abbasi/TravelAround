e8bb12512506698a8048b786d1f5aad8e138fecb
Sarang <99014002+Sarang-Abbasi@users.noreply.github.com>
2026-03-23 18:08:20 +0500

    Change database password in connection string
    Updated database connection string to use a placeholder for the password.

--- TravelAround/db.cs
+++ TravelAround/db.cs
@@ -8,7 +8,7 @@ namespace TravelAround
         // Connection string for MySQL database
         // Contains server address, database name, username, and password
         // This should be changed according to environment (local/production)
-        private static string cs = "server=localhost;database=travelaround;uid=root;pwd=#Skywalker1;";
+        private static string cs = "server=localhost;database=travelaround;uid=root;pwd=YOUR_PASSWORD;";
 
         // Returns a new MySqlConnection object using the connection string
         // This method is reused across all forms and storage classes
